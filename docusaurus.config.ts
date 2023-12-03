79ae14fc5733cc5d3dc61391e2ab630fa3c970a3
Mr Coxall <mr.coxall@mths.ca>
2023-12-03 03:51:20 +0000

    potential fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://mr-coxall.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/raspberry-pi-64-bit-assembly-textbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
